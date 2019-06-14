f14c5953b812b6294d57fbe8060c2f6f6e7e791d
Gosha Maruzhenko <gmaruzhenko@yahoo.ca>
2019-06-14 08:33:26 -0700

    done

--- m-b_distribution_he.py
+++ m-b_distribution_he.py
@@ -5,30 +5,46 @@ import numpy as np
 from numpy import sqrt, sin, cos, pi, e, sqrt, isclose
 import matplotlib.pyplot as plt
 
-na = 6.022 * 10**23
-MOLAR_MASS = 28 # g/mol
+# good place to check
+# http://hyperphysics.phy-astr.gsu.edu/hbase/Kinetic/kintem.html#c2
+
+# speed of sound
+# http://hyperphysics.phy-astr.gsu.edu/hbase/Sound/souspe3.html
+
+na = 6.022 * 10 ** 23
+KG_CONVERT = 1000 # g/kg
+MOLAR_MASS = 4  # g/mol
 m = MOLAR_MASS / 1000 / na  # atomic mass    kg
 k = 1.38064852 * 10 ** -23  # botzman J/K
-TEMP = 273.14 + 20 # K
+R = 8.314  # J/mol /K
+TEMP = 273.14 + 20  # K
 deltaV = 10  # m/s
+gamma_air = 1.4
+gamma_helium = 5./3
+gamma = gamma_helium
 
 
 def f(v):
-    max_distribution = 0.0019897789007185525
-    error = 0.00001
     result = abs(sqrt((m / (2 * pi * k * TEMP))) ** 3) * 4 * pi * v ** 2 * e ** (-1 * m * v ** 2 / (2 * k * TEMP))
-    if isclose(result,max_distribution,error):
-        print(result)
     return result
 
-def find_v_rms(max_dist, velocities_array):
+
+def find_v_rms():
+    most_probable_speed = 1 / sqrt(m / (2 * k * TEMP))
+    v_rms = sqrt(3 * R * TEMP / (MOLAR_MASS / KG_CONVERT))
+    mean_speed = sqrt(8 * R * TEMP / (pi * MOLAR_MASS / KG_CONVERT))
+    speed_sound = sqrt(gamma/3)*v_rms
+    print("vrms = ", v_rms)
+    print("most probable speed = ", most_probable_speed)
+    print("mean speed =", mean_speed)
+    print("speed of sound = ", speed_sound)
     return
 
 
-velocities = np.arange(1,3000,1)
-plt.plot(velocities,f(velocities))
+find_v_rms()
+velocities = np.arange(1, 3000, 1)
+plt.plot(velocities, f(velocities))
 plt.show()
 distribution = f(velocities)
 
 print(max(distribution))
-
